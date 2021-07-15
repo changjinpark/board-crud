bd69e0ebcf7d88777d30d168d3bcc97187a484e5
changjin.park <pcjin118@gmail.com>
2021-07-15 22:04:06 +0900

    JPA 도메인 클래스 생성, JPA 테스트 코드 작성

---
+++ src/main/java/com/steadyjin/board/springboot/domain/posts/Posts.java
@@ -0,0 +1,31 @@
+package com.steadyjin.board.springboot.domain.posts;
+
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Getter
+@Entity
+@NoArgsConstructor
+public class Posts {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long boardId;
+    private String title;
+    private String content;
+    private String author;
+
+    @Builder
+    public Posts(String title, String content, String author){
+        this.title = title;
+        this.content = content;
+        this.author = author;
+    }
+
+}
