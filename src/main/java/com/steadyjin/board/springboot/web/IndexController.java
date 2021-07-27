c42a293e5546e7aa3aae1a164915058250911763
changjin.park <pcjin118@gmail.com>
2021-07-27 20:11:09 +0900

    구글 로그인

--- src/main/java/com/steadyjin/board/springboot/web/IndexController.java
+++ src/main/java/com/steadyjin/board/springboot/web/IndexController.java
@@ -1,5 +1,7 @@
 package com.steadyjin.board.springboot.web;
 
+import com.steadyjin.board.springboot.config.auth.LoginUser;
+import com.steadyjin.board.springboot.config.auth.dto.SessionUser;
 import com.steadyjin.board.springboot.service.posts.PostsService;
 import com.steadyjin.board.springboot.web.dto.PostsReadResponseDto;
 import lombok.RequiredArgsConstructor;
@@ -8,15 +10,25 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import javax.servlet.http.HttpSession;
+
 @RequiredArgsConstructor
 @Controller
 public class IndexController {
 
     private final PostsService postsService;
+    private final HttpSession httpSession;
 
     @GetMapping("/")
-    public String index(Model model) {
+    public String index(Model model, @LoginUser SessionUser user) {
         model.addAttribute("posts", postsService.findAllDesc());
+
+        //SessionUser user = (SessionUser) httpSession.getAttribute("user");
+
+        if(user != null) {
+            model.addAttribute("userName", user.getName());
+            System.out.println(user.getName());
+        }
         return "index";
     }
 
